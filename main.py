748b8b4c60a89b07721b76489d936df04a2cf32a
kanbs <Kartiknagare3165@gmail.com>
2026-09-05 22:51:58 +0530

    basic understanding of api

--- main.py
+++ main.py
@@ -1,24 +1,53 @@
-import requests
+from datetime import date, timedelta
+import httpx
+import asyncio
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 cal_api = os.getenv("CALENDARIFIC_API_KEY")
-print(cal_api) 
-def get_cal():
-    try:
-        data = requests.get(
+
+async def get_festival(api_key: str, start_date: date = date.today(), end_date_days :int = 6) -> list:
+    country = "IN"
+    year = start_date.year
+    end_date = start_date + timedelta(days=end_date_days)
+
+    festival_dates = []
+
+    async with httpx.AsyncClient() as client:
+        all_holidays = await client.get(
             "https://calendarific.com/api/v2/holidays",
             params={
-                "api_key": cal_api, 
-                "country": "US", 
-                "year": 2026
-            },
+                "api_key": api_key,
+                "country": country,
+                "year": year,
+            }
         )
-        # print(r.json()["response"]["holidays"])
-    except Exception as e:
-        print(f"Error {e}")
-    return data.json()
 
-print(get_cal())
+        all_holidays.raise_for_status()
+
+    holidays = all_holidays.json()["response"]["holidays"]
+
+    for week_holidays in holidays:
+        holiday_date = date.fromisoformat(
+            week_holidays["date"]["iso"]
+        )
+
+        if start_date <= holiday_date <= end_date:
+            festival_dates.append({
+                "name": week_holidays["name"],
+                "date": week_holidays["date"]["iso"],
+                "description": week_holidays["description"]
+            })
+
+    return festival_dates
+
+
+async def main():
+    print(await get_festival(
+        api_key=cal_api,
+        end_date_days=25
+    ))
+
+asyncio.run(main())
